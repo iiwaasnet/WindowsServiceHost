e539a640d8d6fc275f17d4c0b50faca311fb887e
iiwaasnet <givasena@gmail.com>
2016-05-29 18:29:42 +0200

    Added helper method to get service name of the running process

--- src/HostEnvironment.cs
+++ src/HostEnvironment.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Management;
 
 namespace WindowsServiceHost
 {
-    internal static class HostEnvironment
+    public static class HostEnvironment
     {
         internal static bool IsRunningAsService()
-           => !Environment.UserInteractive;
+            => !Environment.UserInteractive;
+
+        public static string GetServiceName()
+        {
+            var processId = Process.GetCurrentProcess().Id;
+            var query = $"SELECT * FROM Win32_Service where ProcessId = {processId}";
+            var searcher = new ManagementObjectSearcher(query);
+
+            foreach (var queryObj in searcher.Get())
+            {
+                return queryObj["Name"].ToString();
+            }
+
+            return null;
+        }
     }
 }
